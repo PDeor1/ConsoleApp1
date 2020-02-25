5c5f9a3061a91ca51ba99ea83e8f145db3c0db54
Pankaj Deore <PDeore@slb.com>
2020-02-25 19:22:12 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static string _strTest;
+        
+        static void Main(string[] args)
+        {
+            TestClass obj = new TestClass();
+            obj.iCount = 100000;
+            obj.sName = "SOmething tessssssssssssssssssssssssssssssssssssssssssssss";
+            _strTest = "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy";
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class  TestClass
+    {
+        public int iCount;
+        public string sName;
+    }
+}
